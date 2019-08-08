73ebc662e60a42c9bda37c55a831830659166675
hellozdq <34668661+hellozdq@users.noreply.github.com>
2019-08-08 15:06:06 +0800

    Update toFixed.js
    updata toFixed 2019.8.8

--- test-demo/toFixed.js
+++ test-demo/toFixed.js
@@ -14,7 +14,51 @@ Number.prototype.toFixed=function(n){
 	}
 	return num.toString();
 }
+
+// Number.prototype.toFixed=function(v){
+// 	var n=Number(this); //转成数字
+// 	if(v==undefined){   //v 为空就则默认为0
+// 		v=0;
+// 	}
+// 	var d=Number(v);    
+// 	if(d<=0){           //小于0的当0
+// 		d=0;
+// 	}
+// 	var s=n+"",//转字符串
+// 		a1=s.split(".")[0],//整数
+// 		a2=s.split(".")[1]||"";//小数
+// 		
+// 	if(d===0&&a2.length===0){
+// 		return a1;
+// 	}
+// 	
+// 	l=d-a2.length;
+// 	if(l<0){  //小数比参数长
+// 		var b=a2.substr(d,1);
+// 		if(d===0){ // 参数等于0时
+// 			if(Number(b)>=5){   //(2.5).toFixed(0)
+// 				return Number(a1)+1;
+// 			}
+// 			return a1;
+// 		}
+// 		
+// 		if(Number(b)>=5){ //参数的后一位数大于5
+// 			var max=Number((new Array(d+1)).join("9"));
+// 			var ditValue=(Number(a2.substr(0,d))+1);
+// 			if(ditValue>max){  //小数加1后超过最大位数式则整数部分减1，小数部分取后两位 如(1.99).toFixed(1);
+// 				return (Number(a1)+1)+"."+ditValue.toString().substr(1,d);
+// 			}
+// 			return a1+"."+ditValue
+// 		}
+// 		return a1+'.'+a2.substr(0,d);
+// 	}
+// 	var z= (new Array(l+1)).join("0")//补"0";
+// 	return a1+'.'+a2+z;
+// }
+
+
 console.log((1).toFixed(2));
 console.log((1.115).toFixed(2));
 console.log((1.1115).toFixed(3));
 console.log((1.11115).toFixed(4));
+
